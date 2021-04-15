4d122ac39356d4a072aea87ec09e6e229fc104b9
sumeet bhati <bhatisumit10@gmail.com>
2021-04-15 23:00:17 +0530

    Stored Procedure db first and AuthenticationLogin in .net
    Signed-off-by: sumeet bhati <bhatisumit10@gmail.com>

---
+++ Day_Seven/AuthenticationLogin/AuthenticationLogin/Models/LoginValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationLogin.Models
+{
+    public class LoginValidator
+    {
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage ="Password is required")]
+        [Display(Name ="Password:")]
+        public string password { get; set; }
+        [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "Username:")]
+        public string username { get; set; }
+    }
+    [MetadataType(typeof(LoginValidator))]
+    public partial class tbllogin
+    { 
+    
+    }
+}
